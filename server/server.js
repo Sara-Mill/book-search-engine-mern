b1f4187fcd2af761ee9e41f8b6ba69b5a3119958
Sara-Mill <106774866=sarbear2-0@users.noreply.github.com>
2023-01-08 17:31:51 -0700

    fixed typo in server.js line 31 for Apollo Server

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
-const startAolloServer = async (typeDefs, resolvers) => {
+const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
